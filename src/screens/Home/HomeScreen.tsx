b566ceeeea028b10fbef8bd245989ede161e519a
silviaphungky <silvia.phungky@mandirisek.co.id>
2023-07-04 11:14:26 +0700

    create discover, home, and wallet screen

---
+++ src/screens/Home/HomeScreen.tsx
@@ -0,0 +1,13 @@
+import { SafeAreaView, Text, View } from 'react-native'
+
+const HomeScreen = () => {
+  return (
+    <SafeAreaView>
+      <View style={{ backgroundColor: '#fff', height: '100%' }}>
+        <Text>Home Screen</Text>
+      </View>
+    </SafeAreaView>
+  )
+}
+
+export default HomeScreen
---
+++ src/screens/Wallet/WalletScreen.tsx
@@ -0,0 +1,13 @@
+import { SafeAreaView, Text, View } from 'react-native'
+
+const WalletScreen = () => {
+  return (
+    <SafeAreaView>
+      <View style={{ backgroundColor: '#fff', height: '100%' }}>
+        <Text>Wallet Screen</Text>
+      </View>
+    </SafeAreaView>
+  )
+}
+
+export default WalletScreen
