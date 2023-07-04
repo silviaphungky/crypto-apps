e86203838e042aa0d77704db57377b567359e37d
silviaphungky <silvia.phungky@mandirisek.co.id>
2023-07-04 11:14:07 +0700

    creat account screen

---
+++ src/screens/Account/AccountScreen.tsx
@@ -0,0 +1,13 @@
+import { SafeAreaView, Text, View } from 'react-native'
+
+const AccountScreen = () => {
+  return (
+    <SafeAreaView>
+      <View style={{ backgroundColor: '#fff', height: '100%' }}>
+        <Text>Account Screen</Text>
+      </View>
+    </SafeAreaView>
+  )
+}
+
+export default AccountScreen
