5619812d443926748087cdda2cafdd284d476446
silviaphungky <silvia.phungky@mandirisek.co.id>
2023-07-04 11:10:50 +0700

    setup import path

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,22 @@
-module.exports = function(api) {
-  api.cache(true);
+module.exports = function (api) {
+  api.cache(true)
   return {
     presets: ['babel-preset-expo'],
-  };
-};
+    plugins: [
+      [
+        'module-resolver',
+        {
+          alias: {
+            components: './src/components',
+            icons: './src/components/icons',
+            screens: './src/screens',
+            utils: './src/utils',
+            hooks: './src/hooks',
+            services: './src/services',
+            constants: './src/constants',
+          },
+        },
+      ],
+    ],
+  }
+}
